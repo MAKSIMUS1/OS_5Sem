e943eabc83fb03b21205c86fee557c21a0dc92a9
Maksim <maksimus1132@gmail.com>
2024-09-20 18:16:32 +0300

    Add .gitignore

---
+++ lab3/OS03_02/OS03_02.cpp
@@ -0,0 +1,38 @@
+﻿#include <Windows.h>
+#include <iostream>
+
+int main()
+{
+	STARTUPINFO cif;
+	ZeroMemory(&cif, sizeof(STARTUPINFO));
+	PROCESS_INFORMATION pi;
+
+	std::wstring ApplicationName1(L"OS03_02_1.exe");
+	LPWSTR AppName1 = &ApplicationName1[0];
+
+	if (CreateProcess(AppName1, NULL,
+		NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &cif, &pi) == TRUE)
+	{
+		std::cout << "process" << std::endl;
+		std::wcout << "handle " << pi.hProcess << std::endl;
+	}
+
+	std::wstring ApplicationName2(L"OS03_02_2.exe");
+	LPWSTR AppName2 = &ApplicationName2[0];
+
+	if (CreateProcess(AppName2, NULL,
+		NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &cif, &pi) == TRUE)
+	{
+		std::cout << "process" << std::endl;
+		std::wcout << "handle " << pi.hProcess << std::endl;
+	}
+
+	for (int i = 0; i < 100; i++)
+	{
+		std::cout << GetCurrentProcessId() << std::endl;
+		Sleep(1000);
+	}
+
+	system("pause");
+	return 0;
+}
---
+++ lab3/OS03_02_2/OS03_02_2.cpp
@@ -0,0 +1,12 @@
+﻿#include <windows.h>
+#include <iostream>
+
+int main(void)
+{
+	for (int i = 0; i < 125; i++)
+	{
+		std::cout << GetCurrentProcessId() << std::endl;
+		Sleep(1000);
+	}
+	return 0;
+}
